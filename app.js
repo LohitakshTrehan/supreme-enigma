1feab28b61a50f26eee1feabdfc60dd8a6d5baed
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2020-03-22 18:33:23 +0530

    [start]: Kickstarting file structure, it is currently unstable, the file structure may change in future, after confirming whether i need a static website or a dynamic one.

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+app.use('/cssfiles',express.static(__dirname+'/styles'))
+app.get('/',function (req,res) {
+  res.sendFile('index.html',{root: path.join(__dirname,'/files')});
+});
+
+app.listen(1337,function() {
+  console.log('Listening on leets port');
+});
