27631bf6a64b3bed087f50c06bc212134c78548c
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2020-03-23 23:55:28 +0530

    [start]: defining basic structure of website as an open book, everything should be shades of grey is my initial aim, lets hope it works

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 app.use('/cssfiles',express.static(__dirname+'/styles'))
+app.use('/imgfiles',express.static(__dirname+'/images'))
 app.get('/',function (req,res) {
   res.sendFile('index.html',{root: path.join(__dirname,'/files')});
 });
