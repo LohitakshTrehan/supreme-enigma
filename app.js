ea1ce932c8fd863701f9aa3fa20e1964135b7df7
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2020-04-11 14:17:38 +0530

    [content]: left-content  - finished static left side, merging in develop, will start on right side

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var app = express();
 app.use('/cssfiles',express.static(__dirname+'/styles'))
 app.use('/imgfiles',express.static(__dirname+'/images'))
+app.use('/svgfiles',express.static(__dirname+'/svg'))
 app.get('/',function (req,res) {
   res.sendFile('index.html',{root: path.join(__dirname,'/files')});
 });
