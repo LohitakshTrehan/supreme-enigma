c50cb6470dabcf3d3c4526ae6381629fda28a35d
Lohitaksh Trehan <lohitakshtrehan@gmail.com>
2020-06-25 20:27:22 +0530

    Emergency Commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+const app = express();
+app.use(helmet());
 app.use('/cssfiles',express.static(__dirname+'/styles'))
 app.use('/imgfiles',express.static(__dirname+'/images'))
 app.use('/svgfiles',express.static(__dirname+'/svg'))
